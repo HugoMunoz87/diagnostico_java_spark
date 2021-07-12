dd7facbfd80cc8d5a01c48cb7736d8eb1d567796
HugoMunoz87 <87294355+HugoMunoz87@users.noreply.github.com>
2021-07-12 00:45:38 -0500

    Update Common.java

--- src/main/java/minsait/ttaa/datio/common/Common.java
+++ src/main/java/minsait/ttaa/datio/common/Common.java
@@ -5,7 +5,9 @@ public final class Common {
     public static final String SPARK_MODE = "local[*]";
     public static final String HEADER = "header";
     public static final String INFER_SCHEMA = "inferSchema";
-    public static final String INPUT_PATH = "src/test/resources/data/players_21.csv";
-    public static final String OUTPUT_PATH = "src/test/resources/data/output";
+    public static final String INPUT_PATH = "input.file";
+    public static final String OUTPUT_PATH = "output.directory";
+    public static final String APPLICATION_PROPERTIES_PATH = "src/conf/applicationProperties.properties";
+
 
 }
