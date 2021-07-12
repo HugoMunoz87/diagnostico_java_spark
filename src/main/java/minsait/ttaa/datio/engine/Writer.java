c9f30978e3872e6e4a4ca54f4e1272a357274ad9
HugoMunoz87 <87294355+HugoMunoz87@users.noreply.github.com>
2021-07-12 00:48:42 -0500

    Update Writer.java

--- src/main/java/minsait/ttaa/datio/engine/Writer.java
+++ src/main/java/minsait/ttaa/datio/engine/Writer.java
@@ -1,21 +1,26 @@
 package minsait.ttaa.datio.engine;
 
+import minsait.ttaa.datio.common.PropertiesLoad;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 
+import java.util.Properties;
+
 import static minsait.ttaa.datio.common.Common.*;
+import static minsait.ttaa.datio.common.naming.PlayerInput.nationality;
 import static minsait.ttaa.datio.common.naming.PlayerInput.teamPosition;
 import static org.apache.spark.sql.SaveMode.Overwrite;
 
 abstract class Writer {
+    final static Properties p = PropertiesLoad.getPropertiesFile();
 
     static void write(Dataset<Row> df) {
         df
-                .coalesce(2)
+                .coalesce(1)
                 .write()
-                .partitionBy(teamPosition.getName())
+                .partitionBy(nationality.getName())
                 .mode(Overwrite)
-                .parquet(OUTPUT_PATH);
+                .parquet(p.getProperty(OUTPUT_PATH));
     }
 
 }
